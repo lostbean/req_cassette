3f79b6a52468803501611a423eb5e1c0a1c8fe4d
lostbean <talktoedgar@gmail.com>
2026-06-17 18:35:15 -0300

    chore: release 0.6.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ReqCassette.MixProject do
   def project do
     [
       app: :req_cassette,
-      version: "0.6.0",
+      version: "0.6.1",
       elixir: "~> 1.20",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
